a8560d328f27c1317594e376da6950a572c370d5
alevaldiviezo <73079613+alevaldiviezo@users.noreply.github.com>
2024-09-26 18:06:34 +0100

    Update Home.tsx server adrress

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
     const [message, setMessage] = useState('');
 
     useEffect(() => {
-        fetch('http://localhost:5000/api')
+        fetch('https://serverviteapp.onrender.com/api')
             .then(response => response.json())
             .then(data => setMessage(data.message))
             .catch(error => console.error('Error fetching data:', error));
